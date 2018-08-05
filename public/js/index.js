15a2386ffdd15122bca741bbd592f506dec50c2a
Thangadaurai <thangaduraigct@gmail.com>
2018-08-05 07:03:25 +0530

    init commit

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+
+var socket=io();
+socket.on('connect',function (){console.log('server connected');
+socket.emit('createEmail',{
+  to:'1@g.com',
+  text:'This s send'
+});
+socket.emit('createMsg',{
+  to:'1@g.commsg',
+  text:'This s send msg'
+});
+});
+socket.on('disconnect',function (){console.log('disconnected from server');});
+socket.on('newEmail',function(email){
+  console.log('new mail',email);
+});
+socket.on('newMsg',function(msg){
+  console.log('new Message',msg);
+});
---
+++ server/server.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+const port = process.env.PORT||3000;
+const publicPath = path.join(__dirname,'../public');
+console.log(__dirname+'/../public');
+console.log(publicPath);
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+app.use(express.static(publicPath));
+
+io.on('connection',(socket)=>{
+  console.log('New user connected');
+
+  socket.emit('newEmail', {
+    text:'Hi',
+    from:'Tcs@gmail.com',
+    createdAt:'23'
+  });
+
+  socket.emit('newMsg',{
+    from:'Haha',
+    text:'Mine',
+    createat:'123'
+  });
+
+  socket.on('createEmail',(newEmail)=>{
+    console.log('New mail',newEmail);
+  });
+
+  socket.on('createMsg',(msg)=>{
+    console.log('Msg',msg);
+  });
+
+  socket.on('disconnect',()=>{
+    console.log('User Disconnected');
+  });
+});
+
+server.listen(port,()=>{
+  console.log(`Server running on port ${port}`);
+});
